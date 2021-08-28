d640be4df156a4232001ede8c3d17bd1b134b413
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-08-28 22:36:24 +0700

    Create project v1.0

--- src/components/Cyberbugs/Menu.js
+++ src/components/Cyberbugs/Menu.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import React from "react";
 
 export default function Menu() {
@@ -18,33 +19,47 @@ export default function Menu() {
       </div>
       <div className="control">
         <div>
-          <i className="fa fa-credit-card" />
-          <span>Cyber Board</span>
+          <i className="fa fa-credit-card mr-3" />
+          <NavLink
+            style={{ color: "#000", fontWeight: "bold" }}
+            activeStyle={{ color: "rgb(0, 82, 204)" }}
+            to="/cyberbugs"
+            activeClass="active"
+          >
+            Cyber Board
+          </NavLink>
         </div>
         <div>
-          <i className="fa fa-cog" />
-          <span>Project Settings</span>
+          <i className="fa fa-cog mr-3" />
+          <NavLink
+            style={{ color: "#000", fontWeight: "bold" }}
+            activeStyle={{ color: "rgb(0, 82, 204)" }}
+            to="/projectsettings"
+            activeClass="active"
+          >
+            Project Settings
+          </NavLink>
         </div>
       </div>
       <div className="feature">
         <div>
-          <i className="fa fa-truck" />
+          <i className="fa fa-truck mr-3" />
           <span>Releases</span>
         </div>
         <div>
-          <i className="fa fa-equals" />
+          <i className="fa fa-equals mr-3" />
           <span>Issues and filters</span>
         </div>
         <div>
-          <i className="fa fa-paste" />
+          <i className="fa fa-paste mr-3" />
           <span>Pages</span>
         </div>
         <div>
-          <i className="fa fa-location-arrow" />
+          <i className="fa fa-location-arrow mr-3" />
           <span>Reports</span>
         </div>
         <div>
-          <i className="fa fa-box" />
+          <i className="fa fa-box mr-3" />
           <span>Components</span>
         </div>
       </div>
--- src/pages/CyberBugs/ProjectSettings/ProjectSettings.js
+++ src/pages/CyberBugs/ProjectSettings/ProjectSettings.js
@@ -1,5 +1,78 @@
-import React from "react";
+import React, { useRef } from "react";
+
+import { Editor } from "@tinymce/tinymce-react";
 
 export default function ProjectSettings(props) {
-  return <div>Project Settings</div>;
+  const handleEditorChange = (e) => {
+    console.log("Content was updated:", e.target.getContent());
+  };
+
+  return (
+    <div className="project-settings container">
+      <div>
+        Projects<span>/</span>singularity 1.5
+        <span>/</span>Project Details
+      </div>
+      <h1 className="project-settings__title">Project Details</h1>
+      <form className="container">
+        <div className="form-group">
+          <label>Name</label>
+          <div>
+            <input name="projectName" type="text" className="form-control" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label>URL</label>
+          <div>
+            <input
+              name="projectName"
+              type="text"
+              className="form-control"
+              value="https://www.atlassian.com/software/jira"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Description</label>
+          <div>
+            <Editor
+              initialValue="<p>Initial content</p>"
+              init={{
+                height: 500,
+                menubar: false,
+                plugins: [
+                  "advlist autolink lists link image",
+                  "charmap print preview anchor help",
+                  "searchreplace visualblocks code",
+                  "insertdatetime media table paste wordcount",
+                ],
+                toolbar:
+                  "undo redo | formatselect | " +
+                  "bold italic backcolor | alignleft aligncenter " +
+                  "alignright alignjustify | bullist numlist outdent indent | " +
+                  "removeformat | help",
+              }}
+              onChange={handleEditorChange}
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label>Project Category</label>
+          <select name="categoryId" className="form-control">
+            <option>Software</option>
+            <option>Marketing</option>
+            <option>Business</option>
+          </select>
+        </div>
+        <button
+          style={{ backgroundColor: "rgb(0, 82, 204)" }}
+          type="submit"
+          class="btn btn-primary mt-3"
+        >
+          Save changes
+        </button>
+      </form>
+    </div>
+  );
 }
