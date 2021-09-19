5c767a19e2549693541169cb6f3106a4db676843
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-09-19 13:53:33 +0700

    Edit task v3.0

--- src/components/Cyberbugs/Main/ContentMain.js
+++ src/components/Cyberbugs/Main/ContentMain.js
@@ -1,65 +1,133 @@
-import { GET_TASK_DETAIL_SAGA } from "../../../redux/constants/Cyberbugs/Cyberbug";
+import {
+  GET_TASK_DETAIL_SAGA,
+  UPDATE_STATUS_TASK_SAGA,
+} from "../../../redux/constants/Cyberbugs/Cyberbug";
 import React from "react";
 import { useDispatch } from "react-redux";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export default function ContentMain(props) {
   const dispatch = useDispatch();
   const { projectDetail } = props;
-  const renderCardTaskList = () => {
-    return projectDetail.lstTask?.map((lstTask, index) => {
-      return (
-        <li
-          key={index}
-          className="card"
-          style={{ width: "17rem", height: "auto" }}
-        >
-          <div className="card-header">{lstTask.statusName}</div>
-          <ul className="list-group list-group-flush">
-            {lstTask.lstTaskDeTail.map((task, index) => {
-              return (
-                <li
-                  key={index}
-                  className="list-group-item"
-                  data-toggle="modal"
-                  data-target="#infoModal"
-                  style={{ cursor: "pointer" }}
-                  onClick={() => {
-                    dispatch({
-                      type: GET_TASK_DETAIL_SAGA,
-                      taskId: task.taskId,
-                    });
-                  }}
-                >
-                  <p>{task.taskName}</p>
-                  <div className="block" style={{ display: "flex" }}>
-                    <div className="block-left">
-                      <p className="text-danger">
-                        {task.priorityTask.priority}
-                      </p>
-                    </div>
-                    <div className="block-right">
-                      <div className="avatar-group" style={{ display: "flex" }}>
-                        <div className="avatar">
-                          {task.assigness?.map((member, index) => {
-                            return (
-                              <img
-                                key={index}
-                                src={member.avatar}
-                                alt={member.avatar}
-                              />
-                            );
-                          })}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </li>
-      );
+  const handleDragEnd = (result) => {
+    let { projectId, taskId } = JSON.parse(result.draggableId);
+    console.log("result", result);
+    let { source, destination } = result;
+    if (!result.destination) return;
+
+    if (
+      destination.index === source.index &&
+      destination.droppableId === source.droppableId
+    )
+      return;
+
+    // Gọi API updateStatus
+    dispatch({
+      type: UPDATE_STATUS_TASK_SAGA,
+      taskStatusUpdate: {
+        taskId: taskId,
+        statusId: destination.droppableId,
+        projectId: projectId,
+      },
     });
+  };
+
+  const renderCardTaskList = () => {
+    return (
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {" "}
+        {projectDetail.lstTask?.map((lstTask, index) => {
+          return (
+            <Droppable key={index} droppableId={lstTask.statusId}>
+              {(provided) => {
+                return (
+                  <div
+                    key={index}
+                    className="card"
+                    style={{ width: "17rem", height: "auto" }}
+                  >
+                    <div className="card-header">{lstTask.statusName}</div>
+                    <ul
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className="list-group list-group-flush"
+                      style={{ height: "100%" }}
+                    >
+                      {lstTask.lstTaskDeTail.map((task, index) => {
+                        return (
+                          <Draggable
+                            key={task.taskId.toString()}
+                            index={index}
+                            draggableId={JSON.stringify({
+                              projectId: task.projectId,
+                              taskId: task.taskId,
+                            })}
+                          >
+                            {(provided) => {
+                              return (
+                                <li
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  key={index}
+                                  className="list-group-item"
+                                  data-toggle="modal"
+                                  data-target="#infoModal"
+                                  // style={{ cursor: "pointer" }}
+                                  onClick={() => {
+                                    dispatch({
+                                      type: GET_TASK_DETAIL_SAGA,
+                                      taskId: task.taskId,
+                                    });
+                                  }}
+                                >
+                                  <p>{task.taskName}</p>
+                                  <div
+                                    className="block"
+                                    style={{ display: "flex" }}
+                                  >
+                                    <div className="block-left">
+                                      <p className="text-danger">
+                                        {task.priorityTask.priority}
+                                      </p>
+                                    </div>
+                                    <div className="block-right">
+                                      <div
+                                        className="avatar-group"
+                                        style={{ display: "flex" }}
+                                      >
+                                        <div className="avatar">
+                                          {task.assigness?.map(
+                                            (member, index) => {
+                                              return (
+                                                <img
+                                                  key={index}
+                                                  src={member.avatar}
+                                                  alt={member.avatar}
+                                                />
+                                              );
+                                            }
+                                          )}
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </li>
+                              );
+                            }}
+                          </Draggable>
+                        );
+                      })}
+                      {provided.placeholder}
+                    </ul>
+                  </div>
+                );
+              }}
+            </Droppable>
+          );
+        })}{" "}
+      </DragDropContext>
+    );
   };
   return (
     <div className="content" style={{ display: "flex" }}>
@@ -67,26 +135,3 @@ export default function ContentMain(props) {
     </div>
   );
 }
-
-// <div className="card" style={{ width: "17rem", height: "25rem" }}>
-//       <div className="card-header">SELECTED FOR DEVELOPMENT 2</div>
-//       <ul className="list-group list-group-flush">
-//         <li className="list-group-item">Cras justo odio</li>
-//         <li className="list-group-item">Dapibus ac facilisis in</li>
-//       </ul>
-//     </div>
-//     <div className="card" style={{ width: "17rem", height: "25rem" }}>
-//       <div className="card-header">IN PROGRESS 2</div>
-//       <ul className="list-group list-group-flush">
-//         <li className="list-group-item">Cras justo odio</li>
-//         <li className="list-group-item">Dapibus ac facilisis in</li>
-//       </ul>
-//     </div>
-//     <div className="card" style={{ width: "17rem", height: "25rem" }}>
-//       <div className="card-header">DONE 3</div>
-//       <ul className="list-group list-group-flush">
-//         <li className="list-group-item">Cras justo odio</li>
-//         <li className="list-group-item">Dapibus ac facilisis in</li>
-//         <li className="list-group-item">Vestibulum at eros</li>
-//       </ul>
-//     </div>
